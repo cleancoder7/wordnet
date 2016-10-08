5e19e7285b2780589ab3ce4ca98004fc62c2fa7f
Wahhaj Ali <wahhajj@gmail.com>
2016-10-08 03:33:18 -0400

    Scroll to top of page when selecting a new word to define

--- app/routes/word.js
+++ app/routes/word.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    didTransition() {
+      Ember.$('html, body').animate({ scrollTop: 0 });
+    }
+  },
+
   model(params) {
     return this.store.findRecord('word', params.word);
   }
