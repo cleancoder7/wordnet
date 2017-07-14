ee1c8f3b8b553c1f3a3e4fb651ec758ded4252bd
Wahhaj Ali <wahhajj@gmail.com>
2017-07-14 10:43:01 -0400

    Upgrade to ember 2.14

--- config/environment.js
+++ config/environment.js
@@ -1,26 +1,21 @@
-/* jshint node: true */
+/* eslint-env node */
+'use strict';
 
 module.exports = function(environment) {
-  var ENV = {
+  let ENV = {
     modulePrefix: 'wordnet',
-    environment: environment,
-    baseURL: '/',
+    environment,
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
-    },
-
-    contentSecurityPolicy: {
-      'default-src': "'none'",
-      'script-src': "'self' 'unsafe-inline'",
-      'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com",
-      'font-src': "'self' fonts.gstatic.com",
-      'connect-src': "'self'",
-      'img-src': "'self' data:",
-      'media-src': "'self'"
     },
 
     APP: {
@@ -40,7 +35,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
